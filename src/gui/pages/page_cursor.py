010becb0d0fac86eb7220084dd23817f5817ac82
acidcoke <oliver@hinzmann.xyz>
2023-05-30 20:29:55 +0200

    make window resizable

--- src/gui/pages/page_cursor.py
+++ src/gui/pages/page_cursor.py
@@ -224,8 +224,8 @@ class PageCursor(SafeDisposableFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
-        self.grid_rowconfigure(2, weight=1)
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(2, weight=0)
+        self.grid_columnconfigure(0, weight=0)
         self.is_active = False
         self.task = {}
 
